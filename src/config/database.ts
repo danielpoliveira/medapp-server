17a9ec7fda5bbddfec3d4a389452646b81133157
danielpoliveira <dani.edm@outlook.com>
2020-11-17 01:31:04 -0300

    database changes

--- src/config/database.ts
+++ src/config/database.ts
@@ -10,7 +10,20 @@ export default {
   },*/
 
 
-  database: 'medapp',
+
+  host: 'ec2-100-25-231-126.compute-1.amazonaws.com',
+  database: 'd7tvi7844ljatk',
+  username: 'fepqhnavxlpoqt',
+  port: 5432,
+  password: 'e379b6bdba77ec66cb4866260e805bd7945d5894092ad549cc2aa632234ae3f',
+  dialect: 'postgres',
+  define: {
+    timestamps: true,
+    underscored: true,
+  }
+  
+
+  /*database: 'medapp',
   username: 'me',
   password: '12345',
   dialect: 'postgres',
@@ -18,7 +31,7 @@ export default {
   define: {
     timestamps: true,
     underscored: true,
-  }
+  }*/
 
 }
 
